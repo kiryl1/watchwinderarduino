28be5c7a22370ae2b4a05d65c49478d538580c0f
Kiryl <kirylkul@gmail.com>
2021-02-21 17:40:03 -0500

    fixed folder structure

--- watchwinder.ino
+++ watchwinder.ino
@@ -23,57 +23,88 @@ Stepper stepper(stepsPerRevolution, 8, 10, 9, 11);
 const int ProxSensor=3;
 bool hasrun;
 bool detected;
-int clockthread() {
-  //outputs current time onto lcd
-    DateTime now = rtc.now();
-    lcd.clear();
-    
-    lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    lcd.setCursor(0, 1);
-    lcd.print(now.hour(), DEC);
-    lcd.print(':');
-    lcd.print(now.minute(), DEC);
-    lcd.print(':');
-    lcd.print(now.second(), DEC);
-    lcd.setCursor(0, 2);
-    lcd.print(now.day(), DEC);
-    lcd.print('/');
-    lcd.print(now.month(), DEC);
-    lcd.print('/');
-    lcd.print(now.year(), DEC);
-    lcd.print(' ');
+int DisplayClock()
+{
+  //prints clock to lcd screen
+  DateTime now = rtc.now();
+  lcd.clear();
+  lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  lcd.setCursor(0, 1);
+  lcd.print(now.hour(), DEC);
+  lcd.print(':');
+  lcd.print(now.minute(), DEC);
+  lcd.print(':');
+  lcd.print(now.second(), DEC);
+  lcd.setCursor(0, 2);
+  lcd.print(now.day(), DEC);
+  lcd.print('/');
+  lcd.print(now.month(), DEC);
+  lcd.print('/');
+  lcd.print(now.year(), DEC);
+  lcd.print(' ');
+}
 
+int DisplayProgress(float progress, const TimeSpan& elapsedTime)
+{
+  lcd.clear();
+  lcd.print(progress, 2);
+  lcd.print("% complete");
+  lcd.setCursor(0, 1);
+  lcd.print("Elapsed time:");
+  lcd.setCursor(0, 2);
+  if (elapsedTime.hours() < 10)
+       lcd.print('0');
+  lcd.print(elapsedTime.hours(), DEC);
+  lcd.print(':');
+  if (elapsedTime.minutes() < 10)
+       lcd.print('0');
+  lcd.print(elapsedTime.minutes(),DEC);
+  lcd.print(':');
+  if (elapsedTime.seconds() < 10)
+      lcd.print('0');
+  lcd.print(elapsedTime.seconds(), DEC);
+}
+
+int StartWinding()
+{
+  //turns fully clockwise and fully counter clockwise and updates progress and time elapsed
+  stepper.setSpeed(300);
+
+  enableStepper();
+  DateTime startTime = rtc.now();
   
- 
+  for (int counter = 0; counter < 349; counter++)
+  {
+    DateTime currentTime = rtc.now();
+    TimeSpan elapsedTime = currentTime - startTime;
+    float currentProgress = 100.0 * (float)counter / 350.0;
+   
+    DisplayProgress(currentProgress, elapsedTime);
+
+    int Steps = -2048;
+    //-2048 is one full rotation counter clockwise 
+
+   
+    stepper.step(Steps);
+
+    delay(2 * SECOND);     
+
+    Steps  = 2048;
+
+   
+    stepper.step(Steps);
+
+    delay(2 * SECOND);     
+  }
+
+  disableStepper();
 }
 
 
-int windthread() {
-  // rotates step motor one full rotation clocwise then ccw 
-  //total 700 rotations
-      stepper.setSpeed(300);     
-      enableStepper();
-      for (int counter = 0; counter < 349; counter++) {
-        clockthread();
-        int Steps = -2048;
-        clockthread();
-        stepper.step(Steps);
-        clockthread();
-        delay(2 * SECOND);     
-        clockthread();
-        Steps  = 2048;  
-        clockthread();
-        stepper.step(Steps);
-        clockthread();
-        delay(2 * SECOND);    
-      }
-      disableStepper();
-  
-}
 
 
-
-void enableStepper() {
+void enableStepper()
+{
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
@@ -81,7 +112,8 @@ void enableStepper() {
   digitalWrite(5, HIGH);        
 }
 
-void disableStepper() {
+void disableStepper()
+{
   
   pinMode(8, INPUT);
   pinMode(9, INPUT);
@@ -92,72 +124,73 @@ void disableStepper() {
 
 
 
-bool detecthome(){
-  //detects if IR sensor has been triggered
-    if(digitalRead(ProxSensor)==LOW)      
-             { 
-              detected = true;
-               digitalWrite(13, LOW);
-               //pin 13 is led 
-               return true;
-             }
+bool DetectStartingPosition()
+//detects if IR sensor has been triggered
+{
+  if (digitalRead(ProxSensor) == LOW)   
+  {
+    detected = true;
+    digitalWrite(13, LOW);
+    return true;
+  }
   else
-            {
-              digitalWrite(13, HIGH); 
-              return false;
-             }
-            
-   
+  {
+    digitalWrite(13, HIGH);
+    return false;
+  }
 }
-void gohome(){
-  // moves step motor 10 steps and shows current time in between 
-  stepper.setSpeed(100);     
 
-      enableStepper();
-        clockthread();
-        stepper.step(10);
-           
-      disableStepper();
-      
+void GotoStartingPosition()
+{
+  // moves step motor 10 steps and shows current time in between 
+  stepper.setSpeed(100);
+  enableStepper();
+  stepper.step(10);
+  disableStepper();
 }
+
 void setup()
 {
+  //IR sensor setup
   pinMode(13, OUTPUT);   
-  pinMode(ProxSensor,INPUT);
-  // IR sensor setup 
+  pinMode(ProxSensor, INPUT);
   hasrun = false;
-
-  lcd.setCursor(0, 0);
-  rtc.adjust(DateTime(__DATE__, __TIME__));
-  DateTime now = rtc.now();
+  //lcd setup
   Wire.begin();
-
+  lcd.setCursor(0, 0);
   lcd.begin();
-
   lcd.backlight();
   detected = false;
 }
 
 void loop()
-{DateTime now = rtc.now();
-// spin step motor until we trigger IR sensor which means we have reached home position
-// this is dones so that the rings are alligned in a way which makes it easy to put in the watch into the winder so the rings dont obstruct anything
-if(!hasrun){ 
-   
-   if(!detecthome() ){
-   
-      gohome(); 
-      
+{
+  DisplayClock();
+
+  if (!hasrun)
+  {
+ // spin step motor until we trigger IR sensor which means we have reached home position
+// this is done so that the rings are alligned in a way which makes it easy to put in the watch into the winder so the rings dont obstruct anything
+    if (!DetectStartingPosition())
+    {
+      GotoStartingPosition();
+    }
   }
-}
-  if(!hasrun && detecthome()){
-    delay(60*SECOND);
-  windthread();
-  hasrun = true;
+
+  if (!hasrun && DetectStartingPosition()) {
+   
+    StartWinding();
+    hasrun = true;
   }
-  if(now.hour() == 8 && now.minute() == 30 && now.second() == 0){
+
+  DateTime now = rtc.now();
+  if (now.hour() == 8 && now.minute() == 30 && now.second() == 0)
+  {
     hasrun = false;
   }
-  
 
+  if (hasrun)
+  {
+    delay(SECOND);
+  }
 }
